58f620a9f8477505d10cd8e00b6cbb0cb21f2392
Paul Baca <pbaca110@cnm.edu>
2017-05-09 09:56:18 -0600

    finished eventTag class https://app.asana.com/0/332296370771631/337070545882520

--- php/eventTag.php
+++ php/eventTag.php
@@ -77,8 +77,138 @@ class eventTag implements \jsonSerializable {
 		}
 	$this->eventTagTagId = $newEventTagTagId;
 	}
+/**
+ * inserts EventTag into mySQL
+ * @param \PDO $pdo PDO connection object
+ * @throws \PDOException when mySQL related errors occur
+ * @throws \TypeError if $pdo is not a PDO connection object
+ **/
 
+public function insert(\PDO $pdo) : void {
+	if($this->eventTagEventId !==null){
+		throw(new \PDOException("not a new EventTagEventId"));
+		}
+	$query = "INSERT INTO eventTag(eventTagEventId,eventTagTagId) VALUES(:eventTagEventId,eventTagTagId)";
+	$statement = $pdo->prepare($query);
+$this->eventTagEventId = intval($pdo->lastInsertId());
+	}
 
+/**
+ * deletes this EventTag from mySQL
+ * @param \PDO $pdo PDO connection object
+ * @throws \PDOException when mySQL related errors occur
+ * @throws \TypeError if $pdo is not a PDO connection object
+ **/
+public function delete(\PDO $pdo) : void {
+	if($this->eventTagEventId ===null){
+		throw(new \PDOException("unable to delete a event tag that does not exist"));
+	}
+	$query = "DELETE FROM eventTag WHERE eventTagTagId = :evenTagTagId";
+	$statement = $pdo->prepare($query);
+	$parameters = ["eventTagEventId" => $this->eventTagEventId];
+	$statement->execute($parameters);
+		}
+/**
+ * updates eventTag in mySQL
+ * @param \PDO $pdo connection object
+ * @throws \PDOException when mySQL related errors occur
+ * @throws \TypeError if $pdo is not a PDO connection object
+ **/
 
+public function update(\PDO $pdo) : void {
+	if($this->eventTagEventId ===null){
+		throw(new \PDOException("unable to update a eventTag that does not exist"));
+	}
+$query = "UPDATE eventTag SET eventTagEventId = :eventTagTagId = : evenTagEvent WHERE eventTagTagId =:eventTagEventId";
+$statement = $pdo->prepare($query);
+}
+
+/**
+ * gets eventTag by EventTagEventId
+ * @param \PDO $pdo PDO connection objet
+ * @param int $EventTagEventId eventTag to search for
+ * @return eventTag|null eventTag found or null if not found
+ * @throws \PDOException when mySQL related errors occur
+ * @throws \TypeError when variables are not the correct data type
+ **/
+
+public static function getEventTagByEventTagEventId(\PDO $pdo, int $eventTagEventId) : ?eventTag {
+	if($eventTagEventId <=0){
+		throw(new \PDOException("eventTag id is not positive"));
+	}
+ $query ="SELECT eventTag, eventTagEventId, EventTagTagId FROM eventTag WHERE eventTagEventId =:eventTag";
+$statment = $pdo->prepare($query);
+try {$eventTag = null;
+$statment->setFetchMode(\PDO::FETCH_ASSOC);
+$row = $statment->fetch();
+if($row !==false){
+	$eventTag = new eventTag($row["eventTagEventTagId"],$row["evenTagTagId"]);
+}
+
+}catch(\Exception $exception){
+	throw(new \PDOException($exception->getMessage(),0,$exception));
+}
+return($eventTag);
+}
+/**
+ * gets eventTag by eventTagTagId
+ * @para \PDO $pdo PDO connection object
+ * @param int $eventTagTagId to search by
+ * @return \SplFixedArray splFixedArray of eventTags found
+ * @throws \PDOException when mySQL related errors occur
+ * @throws \TypeError when variables are not the correct data type
+ **/
+
+	public static function getEventTagbyEventTagTagId(\PDO $pdo, int $eventTagTagId): \SplFixedArray {
+		if($eventTagTagId <= 0) {
+			throw(new \RangeException("eventTag must be positive"));
+		}
+		$query = "SELECT eventTag, eventTagEventId, eventTagTagId FROM eventTag WHERE eventTagTagId =:eventTagTagId";
+		$statement = $pdo->prepare($query);
+		$parameters = ["eventTagTagId" => $eventTagTagId];
+		$statement->setFetchMode(\PDO::FETCH_ASSOC);
+		while(($row = $statement->fetch()) !== false) {
+			try {
+				$eventTag = new eventTag($row["eventTag"], $row["eventTagTagId"], $row["eventTagEventId"]);
+				$eventTags[$eventTags->key()] = $eventTag;
+				$eventTags->next();
+			} catch(\Exception $exception) {
+				throw(new \PDOException($exception->getMessage(), 0, $exception));
+			}
+		}
+		return($eventTags);
+	}
+/**
+ * gets all events tags
+ * @param \PDO $pdo PDO connection
+ * @return \SplFixedArray SplFixedArray of eventTags found or null if not found
+ * @throws \PDOException when mySQL related errors occur
+ * @throws \TypeError when variables are not the correct data type
+ **/
+
+public static function getAllEventTags(\PDO $pdo) : \SplFixedArray {
+	$query = "SELECT eventTag, eventTagEventId,EventTagTagId FROM eventTag";
+	$statement = $pdo->prepare($query);
+	$statement->execute();
+	$eventTags = new \SplFixedArray($statement->rowcount());
+	$statement->setFetchMode(\PDO::FETCH_ASSOC);
+	while(($row = $statement->fetch()) !==false){
+		try {
+			$eventTag = new eventTag($row["eventTag"],$row["eventTagEventId"],["eventTagTagId"]);
+			$eventTags[$eventTags->key()]=$eventTag;
+		} catch(\Exception $exception){
+			throw(new \PDOException($exception->getMessage(),0,$exception));
+		}
+	}
+	return($eventTags);
+}
+
+/**
+ * formats the state variables for JSON serialization
+ * @return array resulting state variables to serialize
+ **/
+public function  jsonSerialize() {
+	// TODO: Implement jsonSerialize() method.
+}
 
 }
