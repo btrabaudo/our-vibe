2cb3bf553f0c2cc735bc09246b81460060438872
Paul Baca <pbaca110@cnm.edu>
2017-05-09 13:26:24 -0600

    working on query for tag https://app.asana.com/0/332296370771631/337070545882519

--- php/eventTag.php
+++ php/eventTag.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Edu\Cnm\OurVibe;
 
 require_once("autoload.php");
@@ -14,6 +15,7 @@ class eventTag implements \jsonSerializable {
 	 * @var int $eventTagTagId
 	 **/
 	private $eventTagTagId;
+
 	/**
 	 * constructor for this eventTag
 	 * @param int|null $newEventTagEventId
@@ -23,13 +25,15 @@ class eventTag implements \jsonSerializable {
 	 * @throws \TypeError id data types violate type hints
 	 * @throws \Exception if some other exception occurs
 	 **/
-	public function __construct(?int $newEventTagEventId, ?int $newEventTagTagId = null){try{
-		$this->setEventTagEventId($newEventTagEventId);
-		$this->setEventTagTagId($newEventTagTagId);
+	public function __construct(?int $newEventTagEventId, ?int $newEventTagTagId = null) {
+		try {
+			$this->setEventTagEventId($newEventTagEventId);
+			$this->setEventTagTagId($newEventTagTagId);
 
-	} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception){
-	$exceptionType = get_class($exception);
-	throw(new $exceptionType($exception->getMessage(),0,$exception));}
+		} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
+			$exceptionType = get_class($exception);
+			throw(new $exceptionType($exception->getMessage(), 0, $exception));
+		}
 	}
 
 	/**
@@ -37,31 +41,32 @@ class eventTag implements \jsonSerializable {
 	 * @return int|null value of eventTagEventId
 	 **/
 
-	public function getEventTagEventId() : int {
-		return($this->eventTagEventId);
+	public function getEventTagEventId(): int {
+		return ($this->eventTagEventId);
 	}
+
 	/**
 	 * mutator method for eventTagEventId
 	 * @param int|null $newEventTagEventId
 	 * @throws \RangeException if $newEventTagEventId is not positive
 	 * @throws \TypeError if $newEventTagEventId is not an integer
 	 **/
-	public function setEventTagEventId(?int $newEventTagEventId) : void{
-		if($newEventTagEventId ===null){
+	public function setEventTagEventId(?int $newEventTagEventId): void {
+		if($newEventTagEventId === null) {
 			return;
 		}
-	   if($newEventTagEventId <= 0){
+		if($newEventTagEventId <= 0) {
 			throw(new \RangeException("EventTagEventId is not positive"));
 		}
-	$this->eventTagEventId = $newEventTagEventId;
+		$this->eventTagEventId = $newEventTagEventId;
 	}
 
 	/**
 	 * accessor method for EventTagTagId
 	 * @return int value of EventTagTagId
 	 **/
-	public function getEventTagTagId() : int{
-		return($this->eventTagTagId);
+	public function getEventTagTagId(): int {
+		return ($this->eventTagTagId);
 	}
 
 	/**
@@ -71,93 +76,97 @@ class eventTag implements \jsonSerializable {
 	 * @throws \TypeError if $newEventTagTagId is not an integer
 	 **/
 
-	public function setEventTagTagId(int $newEventTagTagId) : void {
+	public function setEventTagTagId(int $newEventTagTagId): void {
 		if($newEventTagTagId <= 0) {
 			throw(new \RangeException("EventTagTagId is not positive"));
 		}
-	$this->eventTagTagId = $newEventTagTagId;
+		$this->eventTagTagId = $newEventTagTagId;
 	}
-/**
- * inserts EventTag into mySQL
- * @param \PDO $pdo PDO connection object
- * @throws \PDOException when mySQL related errors occur
- * @throws \TypeError if $pdo is not a PDO connection object
- **/
 
-public function insert(\PDO $pdo) : void {
-	if($this->eventTagEventId !==null){
-		throw(new \PDOException("not a new EventTagEventId"));
+	/**
+	 * inserts EventTag into mySQL
+	 * @param \PDO $pdo PDO connection object
+	 * @throws \PDOException when mySQL related errors occur
+	 * @throws \TypeError if $pdo is not a PDO connection object
+	 **/
+
+	public function insert(\PDO $pdo): void {
+		if($this->eventTagEventId !== null) {
+			throw(new \PDOException("not a new EventTagEventId"));
 		}
-	$query = "INSERT INTO eventTag(eventTagEventId,eventTagTagId) VALUES(:eventTagEventId,eventTagTagId)";
-	$statement = $pdo->prepare($query);
-$this->eventTagEventId = intval($pdo->lastInsertId());
+		$query = "INSERT INTO eventTag(eventTagEventId,eventTagTagId) VALUES(:eventTagEventId,eventTagTagId)";
+		$statement = $pdo->prepare($query);
+		$this->eventTagEventId = intval($pdo->lastInsertId());
 	}
 
-/**
- * deletes this EventTag from mySQL
- * @param \PDO $pdo PDO connection object
- * @throws \PDOException when mySQL related errors occur
- * @throws \TypeError if $pdo is not a PDO connection object
- **/
-public function delete(\PDO $pdo) : void {
-	if($this->eventTagEventId ===null){
-		throw(new \PDOException("unable to delete a event tag that does not exist"));
-	}
-	$query = "DELETE FROM eventTag WHERE eventTagTagId = :evenTagTagId";
-	$statement = $pdo->prepare($query);
-	$parameters = ["eventTagEventId" => $this->eventTagEventId];
-	$statement->execute($parameters);
+	/**
+	 * deletes this EventTag from mySQL
+	 * @param \PDO $pdo PDO connection object
+	 * @throws \PDOException when mySQL related errors occur
+	 * @throws \TypeError if $pdo is not a PDO connection object
+	 **/
+	public function delete(\PDO $pdo): void {
+		if($this->eventTagEventId === null) {
+			throw(new \PDOException("unable to delete a event tag that does not exist"));
 		}
-/**
- * updates eventTag in mySQL
- * @param \PDO $pdo connection object
- * @throws \PDOException when mySQL related errors occur
- * @throws \TypeError if $pdo is not a PDO connection object
- **/
-
-public function update(\PDO $pdo) : void {
-	if($this->eventTagEventId ===null){
-		throw(new \PDOException("unable to update a eventTag that does not exist"));
+		$query = "DELETE FROM eventTag WHERE eventTagTagId = :evenTagTagId";
+		$statement = $pdo->prepare($query);
+		$parameters = ["eventTagEventId" => $this->eventTagEventId];
+		$statement->execute($parameters);
 	}
-$query = "UPDATE eventTag SET eventTagEventId = :eventTagTagId = : evenTagEvent WHERE eventTagTagId =:eventTagEventId";
-$statement = $pdo->prepare($query);
-}
 
-/**
- * gets eventTag by EventTagEventId
- * @param \PDO $pdo PDO connection objet
- * @param int $EventTagEventId eventTag to search for
- * @return eventTag|null eventTag found or null if not found
- * @throws \PDOException when mySQL related errors occur
- * @throws \TypeError when variables are not the correct data type
- **/
+	/**
+	 * updates eventTag in mySQL
+	 * @param \PDO $pdo connection object
+	 * @throws \PDOException when mySQL related errors occur
+	 * @throws \TypeError if $pdo is not a PDO connection object
+	 **/
 
-public static function getEventTagByEventTagEventId(\PDO $pdo, int $eventTagEventId) : ?eventTag {
-	if($eventTagEventId <=0){
-		throw(new \PDOException("eventTag id is not positive"));
+	public function update(\PDO $pdo): void {
+		if($this->eventTagEventId === null) {
+			throw(new \PDOException("unable to update a eventTag that does not exist"));
+		}
+		$query = "UPDATE eventTag SET eventTagEventId = :eventTagTagId = : evenTagEvent WHERE eventTagTagId =:eventTagEventId";
+		$statement = $pdo->prepare($query);
 	}
- $query ="SELECT eventTag, eventTagEventId, EventTagTagId FROM eventTag WHERE eventTagEventId =:eventTag";
-$statment = $pdo->prepare($query);
-try {$eventTag = null;
-$statment->setFetchMode(\PDO::FETCH_ASSOC);
-$row = $statment->fetch();
-if($row !==false){
-	$eventTag = new eventTag($row["eventTagEventTagId"],$row["evenTagTagId"]);
-}
 
-}catch(\Exception $exception){
-	throw(new \PDOException($exception->getMessage(),0,$exception));
-}
-return($eventTag);
-}
-/**
- * gets eventTag by eventTagTagId
- * @para \PDO $pdo PDO connection object
- * @param int $eventTagTagId to search by
- * @return \SplFixedArray splFixedArray of eventTags found
- * @throws \PDOException when mySQL related errors occur
- * @throws \TypeError when variables are not the correct data type
- **/
+	/**
+	 * gets eventTag by EventTagEventId
+	 * @param \PDO $pdo PDO connection objet
+	 * @param int $EventTagEventId eventTag to search for
+	 * @return eventTag|null eventTag found or null if not found
+	 * @throws \PDOException when mySQL related errors occur
+	 * @throws \TypeError when variables are not the correct data type
+	 **/
+
+	public static function getEventTagByEventTagEventId(\PDO $pdo, int $eventTagEventId): ?eventTag {
+		if($eventTagEventId <= 0) {
+			throw(new \PDOException("eventTag id is not positive"));
+		}
+		$query = "SELECT eventTag, eventTagEventId, EventTagTagId FROM eventTag WHERE eventTagEventId =:eventTag";
+		$statment = $pdo->prepare($query);
+		try {
+			$eventTag = null;
+			$statment->setFetchMode(\PDO::FETCH_ASSOC);
+			$row = $statment->fetch();
+			if($row !== false) {
+				$eventTag = new eventTag($row["eventTagEventTagId"], $row["evenTagTagId"]);
+			}
+
+		} catch(\Exception $exception) {
+			throw(new \PDOException($exception->getMessage(), 0, $exception));
+		}
+		return ($eventTag);
+	}
+
+	/**
+	 * gets eventTag by eventTagTagId
+	 * @para \PDO $pdo PDO connection object
+	 * @param int $eventTagTagId to search by
+	 * @return \SplFixedArray splFixedArray of eventTags found
+	 * @throws \PDOException when mySQL related errors occur
+	 * @throws \TypeError when variables are not the correct data type
+	 **/
 
 	public static function getEventTagbyEventTagTagId(\PDO $pdo, int $eventTagTagId): \SplFixedArray {
 		if($eventTagTagId <= 0) {
@@ -176,39 +185,40 @@ return($eventTag);
 				throw(new \PDOException($exception->getMessage(), 0, $exception));
 			}
 		}
-		return($eventTags);
+		return ($eventTags);
 	}
-/**
- * gets all events tags
- * @param \PDO $pdo PDO connection
- * @return \SplFixedArray SplFixedArray of eventTags found or null if not found
- * @throws \PDOException when mySQL related errors occur
- * @throws \TypeError when variables are not the correct data type
- **/
 
-public static function getAllEventTags(\PDO $pdo) : \SplFixedArray {
-	$query = "SELECT eventTag, eventTagEventId,EventTagTagId FROM eventTag";
-	$statement = $pdo->prepare($query);
-	$statement->execute();
-	$eventTags = new \SplFixedArray($statement->rowcount());
-	$statement->setFetchMode(\PDO::FETCH_ASSOC);
-	while(($row = $statement->fetch()) !==false){
-		try {
-			$eventTag = new eventTag($row["eventTag"],$row["eventTagEventId"],["eventTagTagId"]);
-			$eventTags[$eventTags->key()]=$eventTag;
-		} catch(\Exception $exception){
-			throw(new \PDOException($exception->getMessage(),0,$exception));
+	/**
+	 * gets all events tags
+	 * @param \PDO $pdo PDO connection
+	 * @return \SplFixedArray SplFixedArray of eventTags found or null if not found
+	 * @throws \PDOException when mySQL related errors occur
+	 * @throws \TypeError when variables are not the correct data type
+	 **/
+
+	public static function getAllEventTags(\PDO $pdo): \SplFixedArray {
+		$query = "SELECT eventTag, eventTagEventId,EventTagTagId FROM eventTag";
+		$statement = $pdo->prepare($query);
+		$statement->execute();
+		$eventTags = new \SplFixedArray($statement->rowcount());
+		$statement->setFetchMode(\PDO::FETCH_ASSOC);
+		while(($row = $statement->fetch()) !== false) {
+			try {
+				$eventTag = new eventTag($row["eventTag"], $row["eventTagEventId"], ["eventTagTagId"]);
+				$eventTags[$eventTags->key()] = $eventTag;
+			} catch(\Exception $exception) {
+				throw(new \PDOException($exception->getMessage(), 0, $exception));
+			}
 		}
+		return ($eventTags);
 	}
-	return($eventTags);
-}
 
-/**
- * formats the state variables for JSON serialization
- * @return array resulting state variables to serialize
- **/
-public function  jsonSerialize() {
-	// TODO: Implement jsonSerialize() method.
-}
+	/**
+	 * formats the state variables for JSON serialization
+	 * @return array resulting state variables to serialize
+	 **/
+	public function jsonSerialize() {
+		// TODO: Implement jsonSerialize() method.
+	}
 
 }
---
+++ php/tag.php
@@ -0,0 +1,141 @@
+<?php
+namespace Edu\Cnm\OurVibe;
+
+class profile implements \JsonSerializable {
+
+	/**
+	 * id for this tag; this is the primary key
+	 * @var int $tagId
+	 */
+	private $tagId;
+
+	/**
+	 * tagName for this tag
+	 * @var string $tagName
+	 **/
+
+	private $tagName;
+
+
+	/**
+	 * constructor for this tag
+	 * @param int|null $newTagId id of this tag or null if new tag
+	 * @param string $newTagName name of the tag
+	 * @throws \InvalidArgumentException if data types are not valid
+	 * @throws \RangeException if data values are out of bounds
+	 * @throws \TypeError if data types violate type hints
+	 * @throws \Exception if some other exception occurs
+	 **/
+
+	public function __construct(?int $newTagId, ?string $newTagName) {
+		try {
+			$this->setTagId($newTagId);
+			$this->setTagName($newTagName);
+		} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
+			$exceptionType = get_class($exception);
+			throw(new $exceptionType($exception->getMessage(), 0, $exception));
+		}
+
+	}
+/**
+ * accessor method for tagId
+ * @return int value of profile id
+ **/
+public function getTagId(): int{
+	return($this->tagId);
+}
+
+/**
+ * mutator method for tag id
+ * @param int|null $newTagId value of new tag id
+ * @throws \RangeException if $newTagId is not positive
+ * @throws \TypeError if $newTagId is not an integer
+ **/
+
+public function setTagId(?int $newTagId): void{
+	if($newTagId ===null){
+		$this->tagId = null;
+		return;
+	}
+	if($newTagId <=0){
+		throw(new \RangeException("tag id is not positive"));
+	}
+	$this->tagId = $newTagId;
+}
+
+/**
+ * accessor method for tagName
+ * @return string value of the tagName
+ **/
+public function getTagName() : ?string {
+	return ($this->tagName);
+}
+/**
+ * mutator method for tagName
+ * @param string $newTagName
+ * @throws \InvalidArgumentException if the tag name is not a string or insecure
+ * @throws \RangeException if the tag name is not exactly 32 characters
+ * @throws \TypeError id the tag name is not a string
+ **/
+
+public function setTagName(?string $newTagName): void {
+	if($newTagName ===null){
+		$this->tagName = null;
+		return;
+	}
+$newTagName = strtolower(trim($newTagName));
+	if(ctype_xdigit($newTagName) ===false){
+		throw(new \RangeException("user tag name is not valid"));
+	}
+	if (strlen($newTagName) !==32) {
+		throw(new \RangeException("user tag name has to be 32"));
+
+	}
+	$this->tagName = $newTagName;
+}
+
+/**
+ * insert tag name into mySQL
+ * @param \PDO $pdo PDO connection object
+ * @throws \PDOException when mySQL related errors occur
+ * @throws \TypeError if $pdo is not a PDO connection object
+ **/
+
+public function insert(\PDO $pdo): void{
+	if($this->tagName !==null){
+		throw(new \PDOException("not a new tag name"));
+	}
+$query
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
