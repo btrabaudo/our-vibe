72ec8b912580e169c4e948f61d5a5cbc86507761
Marcus Lester <mlester3@cnm.edu>
2017-05-12 02:15:46 -0600

    created unit test file for Image class

--- php/classes/Test/ImageTest.php
+++ php/classes/Test/ImageTest.php
@@ -12,28 +12,155 @@ require_once(dirname(__DIR__) . "/autoload.php");
  * this is a complete php unit test of the image class. it's complete because all mySQL/PDO enabled methods are tested for both invalid and valid inputs.
  *
  * @see Image
- * @author marcoder (mlester3@cnm.edu>
+ * @author marcoder <mlester3@cnm.edu>
  **/
 class ImageTest extends OurVibeTest {
 	/**
-	 * Image that created the Image; this is for foreign key relations
+	 *
 	 *
 	 * @var Image image
 	 **/
 	protected $image = null;
 
 	/**
-	 * valid image hash to create the image object to own the test
+	 * valid image id to create the image object to own the test
 	 *
-	 * @var $VALID_HASH
+	 * @var $VALID_IMAGE_ID
 	 */
-	protected $VALID_IMAGE_HASH;
+	protected $VALID_IMAGE_ID;
 
 	/**
-	 * valid salt to use to create the image object to own the test
-	 * @var string $VALID_SALT
-	 */
-	protected $VALID_PROFILE_SALT;
+	 * test inserting a valid Imagee and verify that the actual mySQL data matches
+	 **/
 
+	public function testInsertValidImage(): void {
+		// count the number of rows and save it for later
+		$numRows = $this->getConnection()->getRowCount("image");
+
+		// create a new Image and insert to into mySQL
+		$image = new Image(null, $this->VALID_IMAGE_ID, $this->VALID_CLOUDINARY_ID);
+		//var_dump($Imagee);
+		$image->insert($this->getPDO());
+
+		// grab the data from mySQL and enforce the fields match our expectations
+		$pdoImage = Image::getImageByImageId($this->getPDO(), $image->getImageId());
+		$this->assertSame($numRows + 1, $this->getConnection()->getRowCount("image"));
+		$this->assertSame($pdoImage->getImageId(), $this->VALID_IMAGE_ID);
+		$this->assertSame($pdoImage->getImageCloudinaryId(), $this->VALID_CLOUDINARY_ID);
+	}
+
+	/**
+	 * test inserting a Image that already exists
+	 *
+	 * @expectedException \PDOException
+	 **/
+
+	public function testInsertInvalidImage(): void {
+
+		// create a image with a non null imageId and watch it fail
+		$image = new Image(OurVibeTest::INVALID_KEY, $this->VALID_IMAGE_ID, $this->VALID_CLOUDINARY_ID);
+		$image->insert($this->getPDO());
+	}
+
+	/**
+	 * test inserting a Image, editing it, and then updating it
+	 **/
+
+	public function testUpdateValidImage() {
+
+		// count the number of rows and save it for later
+		$numRows = $this->getConnection()->getRowCount("image");
+
+		// create a new Image and insert to into mySQL
+		$image = new Image(null, $this->VALID_IMAGE_ID, $this->VALID_CLOUDINARY_ID);
+		$image->insert($this->getPDO());
+
+		// edit the Image and update it in mySQL
+		$image->setImageId($this->VALID_IMAGE_ID);
+		$image->update($this->getPDO());
+
+		// grab the data from mySQL and enforce the fields match our expectations
+		$pdoImage = Image::getImageByImageId($this->getPDO(), $image->getImageId());
+		$this->assertSame($numRows + 1, $this->getConnection()->getRowCount("image"));
+		$this->assertSame($pdoImage->getImageId(), $this->VALID_IMAGE_ID);
+		$this->assertSame($pdoImage->getImageCloudinaryId(), $this->VALID_CLOUDINARY_ID);
+	}
+
+	/**
+	 * test updating an Image that does not exist
+	 *
+	 * @expectedException \PDOException
+	 **/
+
+	public function testUpdateInvalidImage() {
+		// create a Image and try to update it without actually inserting it
+		$image = new Image(null, $this->VALID_IMAGE_ID, $this->VALID_CLOUDINARY_ID);
+		$image->update($this->getPDO());
+	}
+
+	/**
+	 * test creating a Image and then deleting it
+	 **/
+
+	public function testDeleteValidImage(): void {
+		// count the number of rows and save it for later
+		$numRows = $this->getConnection()->getRowCount("image");
+
+		// create a new Image and insert to into mySQL
+		$image = new Image(null, $this->VALID_IMAGE_ID, $this->VALID_CLOUDINARY_ID);
+		$image->insert($this->getPDO());
+
+		// delete the Image from mySQL
+		$this->assertSame($numRows + 1, $this->getConnection()->getRowCount("image"));
+		$image->delete($this->getPDO());
+
+		// grab the data from mySQL and enforce the Profile does not exist
+		$pdoImage = Profile::getImagleByImageId($this->getPDO(), $image->getImageId());
+		$this->assertNull($pdoImage);
+		$this->assertSame($numRows, $this->getConnection()->getRowCount("image"));
+	}
+
+	/**
+	 * test deleting a Image that does not exist
+	 *
+	 * @expectedException \PDOException
+	 **/
+
+	public function testDeleteInvalidProfile(): void {
+
+		// create a Profile and try to delete it without actually inserting it
+		$image = new Image(null, $this->VALID_IMAGE_ID, $this->VALID_CLOUDINARY_ID);
+		$image->delete($this->getPDO());
+	}
+
+	/**
+	 * test inserting a Image and regrabbing it from mySQL
+	 **/
+
+	public function testGetValidImageByImageId(): void {
+
+		// count the number of rows and save it for later
+		$numRows = $this->getConnection()->getRowCount("image");
+
+		// create a new Image and insert to into mySQL
+		$image = new Image(null, $this->VALID_IMAGE_ID, $this->VALID_CLOUDINARY_ID);
+		$image->insert($this->getPDO());
+
+		// grab the data from mySQL and enforce the fields match our expectations
+		$pdoImage = Imagee::getImageByImageId($this->getPDO(), $image->getImageId());
+		$this->assertSame($numRows + 1, $this->getConnection()->getRowCount("image"));
+		$this->assertSame($pdoImage->getImageId(), $this->VALID_IMAGE_ID);
+		$this->assertSame($pdoImage->getImageCloudinaryId(), $this->VALID_CLOUDINARY_ID);
+	}
+
+	/**
+	 * test grabbing a Profile that does not exist
+	 **/
+
+	public function testGetInvalidImageByImageId() : void {
+		// grab a image id that exceeds the maximum allowable image id
+		$image = Image::getImageByImageId($this->getPDO(), OurVibeTest::INVALID_KEY);
+		$this->assertNull($image);
+	}
 
 }
