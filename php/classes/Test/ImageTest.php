0a80a2f9ce2a7e3c2a72058647368b0d7245b07a
Marcus Lester <mlester3@cnm.edu>
2017-05-16 16:23:55 -0600

    added asertEquals to Image class

--- php/classes/Test/ImageTest.php
+++ php/classes/Test/ImageTest.php
@@ -86,6 +86,7 @@ class ImageTest extends OurVibeTest {
 		$pdoImage = Image::getImageByImageId($this->getPDO(), $image->getImageId());
 		$this->assertEquals($pdoImage);
 		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("image"));
+		$this->assertEquals($pdoImage->getImageCloudinaryId(), $image->getImageCloudinaryId());
 
 	}
 
