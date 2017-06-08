e0efc4356fc8a18821daa72620d1e4be2a03ae74
Paul Baca <pbaca110@cnm.edu>
2017-06-08 16:45:04 -0600

    finished tag service

--- src/app/services/tag.ts
+++ src/app/services/tag.ts
@@ -13,6 +13,53 @@ export class tagService extends BaseService{
 	private tagUrl = "api/tag/";
 	createTag(tag : tag) : Observable<status>{
 		return(this.http.post(this.tagUrl,tag))
-			.map(BaseService.extractMessage())
+			.map(BaseService.extractMessage)
+			.catch(BaseService.handleError);
+	}
+	editTag(tag : tag) :Observable<status>{
+		return(this.http.put(this.tagUrl,tag))
+			.map(BaseService.extractMessage)
+			.catch(BaseService.handleError);
+	}
+	getTag(tagId : number) : Observable <tag> {
+		return (this.http.get(this.tagUrl + tagId)
+			.map(BaseService.extractData)
+			.catch(BaseService.handleError));
+	}
+
+	getTagByTagId(tagId: number): Observable <tag[]> {
+		return (this.http.get(this.tagUrl + tagId)
+			.map(BaseService.extractData)
+			.catch(BaseService.handleError));
+	}
+	getTagByTagName(tagName: string): Observable <tag []> {
+		return(this.http.get(this.tagUrl + tagName)
+			.map(BaseService.extractData)
+			.catch(BaseService.handleError));
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
