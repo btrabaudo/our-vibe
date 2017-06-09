4ee87ba58ab364af9c70b25ea355e8ae1c7cb0ec
Marcus Lester <mlester3@cnm.edu>
2017-06-09 11:12:05 -0600

    created css and javscript directories

--- static/index.php
+++ static/index.php
@@ -31,12 +31,48 @@
 
 		<!-- Your JavaScript Form Validator -->
 		<script src="js/form-validate.js"></script>
-
-		<title>Our Vibe</title>
-		<!-- google recaptcha-->
-		<script src='https://www.google.com/recaptcha/api.js'></script>
 	</head>
 	<body>
-
+		<ul id="slide-out" class="side-nav">
+			<li><a href="#!">First Sidebar Link</a></li>
+			<li><a href="#!">Second Sidebar Link</a></li>
+			<li class="no-padding">
+				<ul class="collapsible collapsible-accordion">
+					<li>
+						<a class="collapsible-header">Dropdown<i class="material-icons">arrow_drop_down</i></a>
+						<div class="collapsible-body">
+							<ul>
+								<li><a href="#!">First</a></li>
+								<li><a href="#!">Second</a></li>
+								<li><a href="#!">Third</a></li>
+								<li><a href="#!">Fourth</a></li>
+							</ul>
+						</div>
+					</li>
+				</ul>
+			</li>
+		</ul>
+		<ul class="right hide-on-med-and-down">
+			<li><a href="#!">First Sidebar Link</a></li>
+			<li><a href="#!">Second Sidebar Link</a></li>
+			<li><a class="dropdown-button" href="#!" data-activates="dropdown1">Dropdown<i class="material-icons right">arrow_drop_down</i></a></li>
+			<ul id='dropdown1' class='dropdown-content'>
+				<li><a href="#!">First</a></li>
+				<li><a href="#!">Second</a></li>
+				<li><a href="#!">Third</a></li>
+				<li><a href="#!">Fourth</a></li>
+				<ul id="slide-out" class="side-nav">
+					<li><a href="#!">First Sidebar Link</a></li>
+					<li><a href="#!">Second Sidebar Link</a></li>
+				</ul>
+				<a href="#" data-activates="slide-out" class="button-collapse show-on-large"><i class="material-icons">menu</i></a>
+			</ul>
+		</ul>
+		<a href="#" data-activates="slide-out" class="button-collapse"><i class="material-icons">menu</i></a>
+		<ul id="slide-out" class="side-nav fixed">
+			<li><a href="#!">First Sidebar Link</a></li>
+			<li><a href="#!">Second Sidebar Link</a></li>
+		</ul>
+		<a href="#" data-activates="slide-out" class="button-collapse"><i class="material-icons">menu</i></a>
 	</body>
 </html>
