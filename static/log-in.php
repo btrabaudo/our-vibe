d975b417920fcfa088b70d9cdf6030712863802e
Paul Baca <pbaca110@cnm.edu>
2017-06-09 11:22:51 -0600

    put form

---
+++ static/log-in.php
@@ -0,0 +1,27 @@
+<div class="row">
+	<div class="col-md-3"></div>
+	<div class="col-md-6 center-block">
+		<form id="contact-form" class="form-horizontal well" >
+			<div class="form-group">
+				<label for="venue">Venue Email</label>
+				<div class="input-group">
+					<div class="input-group-addon">
+					</div>
+					<input type="text" class="form-control" id="venue" name="venue" placeholder="Venue">
+				</div>
+			</div>
+			<div class="form-group">
+				<label for="password">Password</label>
+				<div class="input-group">
+					<div class="input-group-addon">
+					</div>
+					<input type="password" class="form-control" id="password" name="password" placeholder="Password">
+				</div>
+			</div>
+			<button class="btn btn-success" type="submit"> Log-In</button>
+		</form>
+
+		<div id="output-area"></div>
+
+
+	</div>
