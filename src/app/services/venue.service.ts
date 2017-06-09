b6d0d0b265352c203204a63e7c1f21d251deb6ba
Billy Trabaudo <btrabaudo@gmail.com>
2017-06-09 07:10:27 -0600

    added venue.services

---
+++ src/app/services/venue.service.ts
@@ -0,0 +1,52 @@
+import {Injectable} from "@angular/core";
+import {Http} from "@angular/http";
+import {BaseService} from "./base.service";
+import {Status} from "../classes/status";
+import {Profile, Venue} from "../classes/venue";
+import {Observable} from "rxjs/Observable";
+
+@Injectable ()
+export class ProfileService extends BaseService {
+
+    constructor(protected http:Http) {
+        super(http);
+    }
+
+    // Define the API endpoint
+    private venueUrl = "api/venue/";
+
+    // Reach out to the venue API, and delete the venue in question.
+    deleteVenue(id: number) : Observable<Status> {
+        return(this.http.delete(this.venueUrl + id)
+            .map(BaseService.extractMessage)
+            .catch(BaseService.handleError));
+    }
+
+    // Call to the Venue API, and edit the venue in question.
+    editVenue(venue: Venue) : Observable<Status> {
+        return(this.http.put(this.venueUrl + venue.venueId, venue)
+            .map(BaseService.extractMessage)
+            .catch(BaseService.handleError));
+    }
+
+    // Call to the Profile API, and get a venue object by its id.
+    getVenue(id: number) : Observable<Venue> {
+        return(this.http.get(this.venueUrl + id)
+            .map(BaseService.extractData)
+            .catch(BaseService.handleError));
+    }
+
+    // Call to the API to grab an array of venue based on the user input.
+    getVenueByVenueName(venueName: string) :Observable<Venue[]> {
+        return (this.http.get(this.venueUrl + venueName)
+            .map(BaseService.extractData)
+            .catch(BaseService.handleError));
+    }
+
+    // Call to the venue API, and grab the corresponding venue by its contact.
+    getVenueByVenueContact(venueContact: string) :Observable<Venue> {
+        return(this.http.get(this.venueUrl + venueContact)
+            .map(BaseService.extractData)
+            .catch(BaseService.handleError));
+    }
+}
