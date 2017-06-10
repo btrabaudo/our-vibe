11bb8040bbf1af7342361cf38ce70a7d10c80486
Marcus Lester <mlester3@cnm.edu>
2017-06-10 09:15:04 -0600

    added search field to navbar

--- static/navbar.php
+++ static/navbar.php
@@ -35,17 +35,27 @@
 	</head>
 	<body>
 		<ul id="slide-out" class="side-nav fixed">
-			<li><div class="userView">
+			<li>
+				<div class="userView">
 					<div class="background">
 						<img src="MAYBE PUT IMAGE HERE">
 					</div>
 					<a href="#!user"><img class="circle" src="MAYBE PUT IMAGE HERE"></a>
 					<a href="#!name"><span class="white-text name">John Doe</span></a>
 					<a href="#!email"><span class="white-text email">jdandturk@gmail.com</span></a>
+			<li class="search">
+				<div class="search-wrapper card">
+					<input id="search">
+					<i class="material-icons">search</i>
+					<div class="search-results"></div>
 				</div>
+			</li>
+			</div>
 
 			<li><a class="waves-effect" href="#!">Our Vibe</a></li>
-			<li><div class="divider"></div></li>
+			<li>
+				<div class="divider"></div>
+			</li>
 			<li><a class="subheader">Something Cool lol</a></li>
 			<li><a class="waves-effect" href="#!">Events</a></li>
 			<li><a class="waves-effect" href="#!">Venues</a></li>
