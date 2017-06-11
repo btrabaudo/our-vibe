5ebb3925330ec14e0c96f7e7dd73b78228681dd0
Billy Trabaudo <btrabaudo@gmail.com>
2017-06-11 15:06:20 -0600

    sign-up component done

--- src/app/components/sign-up.component.ts
+++ src/app/components/sign-up.component.ts
@@ -1,7 +1,20 @@
 import {Component} from "@angular/core";
+import {SignUpService} from "../services/sign-up.service";
+import {SignUp} from "../classes/sign-up";
+import {Status} from "../classes/status";
 
 @Component({
     templateUrl: "./templates/sign-up.php"
 })
 
-export class SignUpComponent {}
+export class SignUpComponent {
+    signUp : SignUp = new SignUp(null, null, null, null, null, null, null, null, null, null, null, null, null);
+    status : Status = null;
+    constructor(private signUpService: SignUpService) {}
+
+
+    createSignUp() : void {
+        this.signUpService.createSignUp(this.signUp)
+            .subscribe(status => this.status = status);
+    }
+}
