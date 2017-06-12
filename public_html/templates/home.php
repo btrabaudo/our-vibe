f4a258730eb3bfd3900f9172f067d88ee08c526b
Billy Trabaudo <btrabaudo@gmail.com>
2017-06-12 09:26:52 -0600

    added navbar to angular home, added css

--- public_html/templates/home.php
+++ public_html/templates/home.php
@@ -1,17 +1,18 @@
+<div class="sfooter">
+    <div class="sfooter-content">
+        <navbar></navbar>
 
-		<div class="sfooter-content">
-            <navbar></navbar>
-
-			<main class="bg p-t-nav">
-				<div class="container">
-					<div class="row">
-						<div class="col-md-8 col-md-offset-4">
-							<div class="jumbotron text-right">
-								<h1>Our Vibe</h1>
-								<p class="lead">Albuquerque, NM</p>
-							</div>
-						</div>
-					</div>
-				</div>
-			</main>
-		</div><!--/.sfooter-content-->
+        <main class="bg p-t-nav">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8 col-md-offset-4">
+                        <div class="jumbotron text-right">
+                            <h1>Our Vibe</h1>
+                            <p class="lead">Albuquerque, NM</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
+    </div><!--/.sfooter-content-->
+</div>
