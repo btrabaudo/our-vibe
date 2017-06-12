7f70641a5d59dccc43880be9f861d75664c753bb
kellykristl <goddesswebdesign@gmail.com>
2017-06-12 13:02:47 -0600

    working on home page and its css

--- static/home.php
+++ static/home.php
@@ -6,18 +6,70 @@
 		<!-- insert header and navbar -->
 		<?php require_once("lib/header.php");?>
 
-		<main class="bg p-t-nav">
-			<div class="container">
-				<div class="row">
-					<div class="col-md-8 col-md-offset-4">
-						<div class="jumbotron text-right">
-							<h1>Our Vibe</h1>
-							<p class="lead">Albuquerque, NM</p>
-						</div>
-					</div>
+		<!-- Page Content -->
+		<div class="container">
+
+			<!-- Page Heading -->
+			<div class="row">
+				<div class="col-lg-12">
+					<h1 class="page-header">Our Vibe
+						<small>Events in Albuquerque, NM</small>
+					</h1>
 				</div>
 			</div>
-		</main>
+			<!-- /.row -->
+
+			<!-- Event One -->
+			<div class="row">
+				<div class="col-md-7">
+					<a href="#">
+						<img class="img-responsive" src="http://placehold.it/700x300" alt="">
+					</a>
+				</div>
+				<div class="col-md-5">
+					<h3>Event One</h3>
+					<h4>text here</h4>
+					<p>This will be like a 2 sentence blurb about the event.</p>
+					<a class="btn btn-primary" href="#">View This Event <span class="glyphicon glyphicon-chevron-right"></span></a>
+				</div>
+			</div>
+			<!-- /.row -->
+
+			<hr>
+
+			<!-- Event Two -->
+			<div class="row">
+				<div class="col-md-7">
+					<a href="#">
+						<img class="img-responsive" src="http://placehold.it/700x300" alt="">
+					</a>
+				</div>
+				<div class="col-md-5">
+					<h3>Event Two</h3>
+					<h4>text here</h4>
+					<p>This will be like a 2 sentence blurb about the event.</p>
+					<a class="btn btn-primary" href="#">View This Event <span class="glyphicon glyphicon-chevron-right"></span></a>
+				</div>
+			</div>
+			<!-- /.row -->
+
+			<hr>
+
+			<!-- Event Three -->
+			<div class="row">
+				<div class="col-md-7">
+					<a href="#">
+						<img class="img-responsive" src="http://placehold.it/700x300" alt="">
+					</a>
+				</div>
+				<div class="col-md-5">
+					<h3>Event Three</h3>
+					<h4>text here</h4>
+					<p>This will be like a 2 sentence blurb about the event.</p>
+					<a class="btn btn-primary" href="#">View This Event <span class="glyphicon glyphicon-chevron-right"></span></a>
+				</div>
+			</div>
+			<!-- /.row -->
 	</div><!--/.sfooter-content-->
 
 	<!-- insert footer -->
