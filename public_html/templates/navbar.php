920f6629ea1f7439f86580f83dc2f87cbb9c536c
Marcus Lester <mlester3@cnm.edu>
2017-06-13 11:26:36 -0600

    new navbar

--- public_html/templates/navbar.php
+++ public_html/templates/navbar.php
@@ -1,73 +1,54 @@
 
-	<!-- Your JavaScript Form Validator -->
-	<script src="javascript/navbar-style.js"></script>
-
-	<body>
-		<div id="wrapper">
+<div id="wrapper">
 
 
-			<!-- Sidebar -->
-			<div id="sidebar-wrapper">
-				<ul class="sidebar-nav">
-					<li class="sidebar-brand">
-						<a href="#">
-							Our Vibe
-						</a>
-					<li class="search">
-						<div class="search-wrapper card">
-							<input id="search">
-							<i class="material-icons">search</i>
-							<div class="search-results"></div>
-						</div>
+	<!-- Sidebar -->
+	<div id="sidebar-wrapper">
+		<ul class="sidebar-nav">
+			<li class="sidebar-brand">
+				<a href="#">
+					OurVibe
+				</a>
+			</li>
+
+			<!-- search bar goes here -->
+			<li class="search">
+				<div class="search-wrapper card">
+					<input id="search">
+					<i class="material-icons">search</i>
+					<div class="search-results"></div>
+				</div>
 
 
-					<li>
-						<a class="waves-effect" routerLink="">Events</a>
-					</li>
-					<li>
-						<a class="waves-effect" routerLink="/venues">Venues</a>
-					</li>
-					<li>
-						<a class="waves-effect" routerLink="/about">About</a>
-					</li>
-					<li>
-						<a class="waves-effect" routerLink="/sign-up">Sign Up</a> / <a class="waves-effect" routerLink="/log-in">Sign In</a>
-					</li>
-				</ul>
-			</div>
-			<!-- /#sidebar-wrapper -->
+
+			<li>
+				<a class="waves-effect" routerLink="">Events</a>
+			</li>
+			<li>
+				<a class="waves-effect" routerLink="/venues">Venues</a>
+			</li>
+			<li>
+				<a class="waves-effect" routerLink="/about">About</a>
+			</li>
+			<li>
+				<a class="waves-effect" routerLink="/sign">Sign Up/Sign In</a>
+			</li>
+		</ul>
+	</div>
+	<!-- /#sidebar-wrapper -->
 
 
-			<!-- Page Content -->
-			<div id="page-content-wrapper">
-				<div class="container-fluid">
-					<div class="row">
-						<div class="col-lg-12">
-							<a href="#menu-toggle" class="btn btn-default" id="menu-toggle">Toggle Menu</a>
-						</div>
-					</div>
+	<!-- Page Content -->
+	<div id="page-content-wrapper">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-lg-12">
+					<a href="#menu-toggle" class="btn btn-default" id="menu-toggle">Toggle Menu</a>
 				</div>
 			</div>
-			<!-- /#page-content-wrapper -->
-
-
 		</div>
-		<!-- /#wrapper -->
+	</div>
+	<!-- /#page-content-wrapper -->
 
 
-		<!-- jQuery -->
-		<script src="js/jquery.js"></script>
-
-
-		<!-- Bootstrap Core JavaScript -->
-		<script src="js/bootstrap.min.js"></script>
-
-
-		<!-- Menu Toggle Script -->
-		<script>
-			$("#menu-toggle").click(function(e) {
-				e.preventDefault();
-				$("#wrapper").toggleClass("toggled");
-			});
-		</script>
-
+</div>
--- static/navbar.php
+++ static/navbar.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html>
+
 	<head>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
@@ -11,12 +12,12 @@
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
 		<!-- Custom CSS -->
-		<link rel="stylesheet" href="src/app.css">
+		<link rel="stylesheet" href="css/style.css">
 
 
 		<!-- jQuery -->
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
-		<script src="javascript/navbar-style.js"></script>
+		<script src="javascript/style.js"></script>
 		<!-- Latest compiled and minified JavaScript -->
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
 				  integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
@@ -33,76 +34,66 @@
 		<!-- Your JavaScript Form Validator -->
 		<script src="javascript/navbar-style.js"></script>
 	</head>
-	<body>
-		<div id="wrapper">
+	<nav>
+	<div id="wrapper">
 
 
-			<!-- Sidebar -->
-			<div id="sidebar-wrapper">
-				<ul class="sidebar-nav">
-					<li class="sidebar-brand">
-						<a href="#">
-							Our Vibe
-						</a>
-					<li class="search">
-						<div class="search-wrapper card">
-							<input id="search">
-							<i class="material-icons">search</i>
-							<div class="search-results"></div>
-						</div>
+		<!-- Sidebar -->
+		<div id="sidebar-wrapper">
+			<ul class="sidebar-nav">
+				<li class="sidebar-brand">
+					<a href="#">
+						OurVibe
+					</a>
+				</li>
+
+				<!-- searchbar goes here -->
 
 
-					<li>
-						<a class="waves-effect" routerLink="">Events</a>
-					</li>
-					<li>
-						<a class="waves-effect" routerLink="/venues">Venues</a>
-					</li>
-					<li>
-						<a class="waves-effect" routerLink="/about">About</a>
-					</li>
-					<li>
-						<a class="waves-effect" routerLink="/sign-up">Sign Up</a> / <a class="waves-effect" routerLink="/log-in">Sign In</a>
-					</li>
-				</ul>
-			</div>
-			<!-- /#sidebar-wrapper -->
 
 
-			<!-- Page Content -->
-			<div id="page-content-wrapper">
-				<div class="container-fluid">
-					<div class="row">
-						<div class="col-lg-12">
-							<a href="#menu-toggle" class="btn btn-default" id="menu-toggle">Toggle Menu</a>
-						</div>
+				<li>
+					<a class="waves-effect" routerLink="">Events</a>
+				</li>
+				<li>
+					<a class="waves-effect" routerLink="/venues">Venues</a>
+				</li>
+				<li>
+					<a class="waves-effect" routerLink="/venues">About</a>
+				</li>
+				<li>
+					<a class="waves-effect" routerLink="/venues">Sign In/Sign Up</a>
+				</li>
+			</ul>
+		</div>
+		<!-- /#sidebar-wrapper -->
+
+
+		<!-- Page Content -->
+		<div id="page-content-wrapper">
+			<div class="container-fluid">
+				<div class="row">
+					<div class="col-lg-12">
+						<a href="#menu-toggle" class="btn btn-default" id="menu-toggle">Toggle Menu</a>
 					</div>
 				</div>
 			</div>
-			<!-- /#page-content-wrapper -->
-
-
 		</div>
-		<!-- /#wrapper -->
+		<!-- /#page-content-wrapper -->
 
 
-		<!-- jQuery -->
-		<script src="js/jquery.js"></script>
-
-
-		<!-- Bootstrap Core JavaScript -->
-		<script src="js/bootstrap.min.js"></script>
-
-
-		<!-- Menu Toggle Script -->
-		<script>
-			$("#menu-toggle").click(function(e) {
-				e.preventDefault();
-				$("#wrapper").toggleClass("toggled");
-			});
-		</script>
+	</div>
+	<!-- /#wrapper -->
 
 
 
-	</body>
+
+	<!-- Menu Toggle Script -->
+	<script>
+		$("#menu-toggle").click(function(e) {
+			e.preventDefault();
+			$("#wrapper").toggleClass("toggled");
+		});
+	</script>
+		</nav>
 </html>
