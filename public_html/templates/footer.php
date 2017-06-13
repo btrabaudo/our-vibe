46e2309835c6e7524f43e4132d28cb76426d6ab3
kellykristl <goddesswebdesign@gmail.com>
2017-06-13 14:59:47 -0600

    footer.php

--- public_html/templates/footer.php
+++ public_html/templates/footer.php
@@ -1,9 +1,11 @@
 <!-- Footer -->
 <footer>
-	<div class="row">
+	<div class="container">
+		<div class="row">
 		<div class="col-lg-12">
 			<p>Copyright &copy; Our Vibe 2017</p>
 		</div>
+	</div>
 	</div>
 	<!-- /.row -->
 </footer>
