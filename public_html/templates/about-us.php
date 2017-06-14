e6a3c20a8df857f9fbe48226026a289b8db5a02c
Billy Trabaudo <btrabaudo@gmail.com>
2017-06-14 11:18:20 -0600

    added to the about page

--- public_html/templates/about-us.php
+++ public_html/templates/about-us.php
@@ -1,2 +1,7 @@
 <h1 class="text-center">Our Vibe</h1>
 <h2 class="text-center">About Us</h2>
+<p>Our Vibe is collective of theatres, music venues, and galleries who have come together to create a one stop shop for their events. Our Vibe is created by a group of artists who also have a passion for coding. We hope that our project will contribute to the common good.</p>
+<h3>Marcus Lester:</h3> <p>mlester3@cnm.edu</p>
+<h3>Paul Baca:</h3> <p>pbaca110@cnm.edu</p>
+<h3>Kelly Kristl:</h3> <p>kkristl@cnm.edu</p>
+<h3>Billy Trabaudo:</h3> <p>btrabaudo@cnm.edu</p>
